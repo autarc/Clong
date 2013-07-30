49f966467cb56983ccd15d4c8a3911646de03560
Autarc <Autarc@gmail.com>
2013-07-30 15:14:43 +0200

    rework player

--- public/code/player.js
+++ public/code/player.js
@@ -1,23 +1,35 @@
 // define([], function(){
 (function(){
 
-	init_player = function () {
-		// create the cube's material
-	    var cubeMaterial = new THREE.MeshLambertMaterial(
-	    {
-	        color: clong.color
-	    });
+  var maxPlayers = 4;
 
-	    // set up the cube vars
-	    var cube_width = 100, cube_height = 100, cube_depth = 100, widthSegments = 10, heightSegments = 10, depthSegments = 10;
+  var part = clong.config.size.width / maxPlayers;
 
-	    // create a new mesh with cube geometry
-	    cube = new THREE.Mesh(
-	       new THREE.CubeGeometry(cube_width, cube_height, cube_depth, widthSegments, heightSegments, depthSegments), 
-	       cubeMaterial);
+  clong.players = [];
 
-	    // add the cube to the scene
-	    scene.add(cube);
-	}
+  function Player ( id, color ) {
+
+    this.echoes = [];
+
+    // create the cube's material
+    var cubeMaterial = new THREE.MeshLambertMaterial({ color: color });
+
+    // set up the cube vars
+    var cube_width     = 100,
+        cube_height    = 100,
+        cube_depth     = 100,
+        widthSegments  =  10,
+        heightSegments =  10,
+        depthSegments  =  10;
+
+    var geometry = new THREE.CubeGeometry(cube_width, cube_height, cube_depth, widthSegments, heightSegments, depthSegments ),
+
+    var cube = new THREE.Mesh( geometry, cubeMaterial );
+
+    cube.position.x = ( id * part ) - cube_width/2;
+    cube.position.y = cube_height/2;
+
+    scene.add(cube);
+  }
 
 })();
