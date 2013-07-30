31800b9af3d0e5a1970aa6fcf24d072c27351df6
Autarc <Autarc@gmail.com>
2013-07-30 15:19:38 +0200

    create circles on input

--- public/code/input.js
+++ public/code/input.js
@@ -18,35 +18,50 @@
     cube.position.x+=5;
   });
 
+
   KeyboardJS.on('c', function() {
 
-  	resolution = 100;
-	var amplitude = 100;
-	size = 360 / resolution;
+  	var resolution = 100,
+  	    amplitude  = 100,
+  	    size = 360 / resolution;
 
-  	var geometry = new THREE.Geometry();
-	material = new THREE.LineBasicMaterial( { color: clong.color, opacity: 1.0, linewidth: 3} );
-	for(var i = 0; i <= resolution; i++) {
-	    var segment = ( i * size ) * Math.PI / 180;
-	    geometry.vertices.push(new THREE.Vector3( Math.cos( segment ) * amplitude, Math.sin( segment ) * amplitude, 0 ));         
-	}
+    var geometry = new THREE.Geometry(),
 
-	var new_line = new THREE.Line( geometry, material );
+  	    material = new THREE.LineBasicMaterial( { color: clong.color, opacity: 1.0, linewidth: 3} );
 
-	new_line.position = cube.position.clone();
+  	for(var i = 0; i <= resolution; i++) {
 
-	new_line.amplitude = amplitude;
+       var segment = ( i * size ) * Math.PI / 180;
+  	   geometry.vertices.push(new THREE.Vector3( Math.cos( segment ) * amplitude, Math.sin( segment ) * amplitude, 0 ));
+  	}
 
-	lines_array.push(new_line);
+  	var new_line = new THREE.Line( geometry, material );
 
-	scene.add(new_line);
+  	new_line.position = cube.position.clone();
+
+  	new_line.amplitude = amplitude;
+
+  	lines_array.push(new_line);
+
+  	scene.add(new_line);
 
   });
 
 
   // Mouse
 
-
   // Controller
 
 })();
+
+
+
+
+
+// if ( !navigator.gamepads ) {
+
+//   navigator.gamepads = navigator.webkitGamepads || navigator.mozGamepads; // || navigator.webkitGetGamepads
+// }
+
+// if ( !navigator.gamepads ) return alert('NO GAMEPAD SUPPORT');
+
